bcd40bedbea41159900c5df7b7de9ae23f0c3003
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-03-17 16:21:12 +0100

    feat(layout): add padding to categories

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -7,7 +7,7 @@ function Layout() {
     <div className="bg-primary-bg-1 text-primary-text-1 flex min-h-screen flex-col transition-colors duration-100">
       <Navbar />
       <div className="flex flex-1">
-        <div className="bg-primary-bg-2 text-primary-text-2 hidden md:flex md:flex-col md:items-start md:gap-1 md:pl-2 md:pt-1">
+        <div className="bg-primary-bg-2 text-primary-text-2 hidden md:flex md:flex-col md:items-start md:gap-1 md:p-2 md:pt-1">
           <p className="font-bold">Kategorien</p>
           <NavBarCategories />
         </div>

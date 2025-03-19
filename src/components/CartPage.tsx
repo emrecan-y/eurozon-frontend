8a33eecd16539e1022a9bb8c91b59c7fd6631e2a
TheDarkestCat <caprino.marco@outlook.com>
2025-03-19 16:10:43 +0100

    (ShoppingCart) updated design

--- src/components/CartPage.tsx
+++ src/components/CartPage.tsx
@@ -5,6 +5,7 @@ import {
 } from "./queries/useShoppingCartQuerys";
 import { Image } from "lucide-react";
 import { useAddOrders } from "./queries/useOrdersQuery";
+import { useEffect, useState } from "react";
 
 export const CartPage = () => {
   const {
@@ -15,6 +16,23 @@ export const CartPage = () => {
 
   const removeMutation = useRemoveShoppingCartPosition();
   const orderMutation = useAddOrders();
+  const [total, setTotal] = useState<number>(0);
+  const [amount, setAmount] = useState<number>(0);
+
+  useEffect(() => {
+    setTotal(0);
+    const sum = shoppingCartList
+      ?.map((e) => Number.parseFloat(e.product.price))
+      .reduce((a, b) => a + b);
+    setAmount(shoppingCartList?.length)
+    if (sum) {
+      setTotal(sum);
+    }
+    // setTotal(sum);
+    // shoppingCartList?.forEach((element) => {
+    //   setTotal(total + element.amount * Number.parseInt(element.product.price));
+    // });
+  }, [shoppingCartList]);
 
   const handleRemoveButton = (cartId: UUID) => {
     removeMutation.mutate(cartId);
@@ -32,15 +50,15 @@ export const CartPage = () => {
   }
 
   return (
-    <div className=" flex flex-col h-full w-full items-center justify-center gap-6 my-12">
+    <div className="my-12 flex h-full w-full flex-col items-center justify-center gap-6">
       {shoppingCartList.map((cartItem) => (
         <div
           key={cartItem.id}
-          className="flex flex-row w-3/5 h-64 items-center justify-center bg-primary-bg-2 p-4 shadow-xl"
+          className="flex h-64 w-3/5 flex-row items-center justify-center rounded-2xl bg-primary-bg-2 p-4 shadow-xl"
         >
           <img
             src={cartItem.product.scrUrl}
-            className=" text-primary-text-2 h-4/5 pl-12"
+            className="h-4/5 pl-12 text-primary-text-2"
           />
           <div className="flex w-full flex-col items-center justify-center gap-3 rounded-lg p-2 px-6 align-middle">
             <p className="font-bold">{cartItem.product.name}</p>
@@ -48,7 +66,7 @@ export const CartPage = () => {
             <p className="font-bold">{cartItem.amount}</p>
             <p className="font-bold">{cartItem.product.price}€</p>
             <button
-              className="rounded-md bg-red-700 p-2"
+              className="rounded-md bg-red-700 p-2 transition-all hover:bg-red-600"
               onClick={() => handleRemoveButton(cartItem.product.id)}
             >
               Item Entfernen
@@ -56,9 +74,16 @@ export const CartPage = () => {
           </div>
         </div>
       ))}
-      <button className="rounded-md bg-green-700 p-2" onClick={handleBuyButton}>
-        Kauf Abschließen
-      </button>
+      <div className="flex flex-col justify-center items-center gap-4 rounded-2xl border bg-white shadow-lg p-12">
+        <p className="font-bold">Gesamtpreis: {total} €</p>
+        <p className="font-semibold">Artikelanzahl: {amount}</p>
+        <button
+          className="rounded-md bg-green-700 p-2 transition-all hover:bg-green-600"
+          onClick={handleBuyButton}
+        >
+          Kauf Abschließen
+        </button>
+      </div>
     </div>
   );
 };

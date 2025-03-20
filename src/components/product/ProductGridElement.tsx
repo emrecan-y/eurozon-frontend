10ec56cd03b9a6f7289b6c90c219ce160570b52c
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-03-20 15:35:55 +0100

    feat(ProductGridElement): change styling

--- src/components/product/ProductGridElement.tsx
+++ src/components/product/ProductGridElement.tsx
@@ -1,7 +1,7 @@
 import { Product } from "@/models/product";
 import { useAddShoppingCartPosition } from "../queries/useShoppingCartQuerys";
 import { UUID } from "crypto";
-import { Image, ShoppingCart } from "lucide-react";
+import { Image, Minus, Plus, ShoppingCart } from "lucide-react";
 import { useState } from "react";
 import {
   Tooltip,
@@ -36,74 +36,88 @@ function ProductGridElement({ product }: ProductGridElement) {
   if (product) {
     return (
       <TooltipProvider>
-      <div className="flex w-full flex-col items-center gap-3 rounded-lg border-2 border-primary-bg-3 bg-primary-bg-2 p-4 shadow-md">
-        <div className="flex w-full items-center justify-center bg-primary-bg-2">
-          <img
-            src={product.imageUrl}
-            className={`h-5/6 text-primary-text-2 ${!imageLoaded && "hidden"}`}
-            onLoad={() => {
-              setImageLoaded(true);
-            }}
-            onError={() => {}}
-          />
-          {!imageLoaded && (
-            <Image className="h-5/6 w-5/6 text-primary-text-2" />
-          )}
+        <div className="flex w-full flex-col items-center gap-3 rounded-lg border-2 border-primary-bg-3 bg-primary-bg-2 p-3 shadow-md">
+          <div className="col-span-3 flex h-full w-full items-center justify-center bg-primary-bg-2 bg-white">
+            <img
+              src={product.imageUrl}
+              className={`h-full w-full object-contain text-primary-text-2 ${!imageLoaded && "hidden"}`}
+              onLoad={() => {
+                setImageLoaded(true);
+              }}
+              onError={() => {}}
+            />
+            {!imageLoaded && (
+              <Image className="h-5/6 w-5/6 text-primary-text-2" />
+            )}
+          </div>
+          <div className="flex w-full">
+            <div className="flex w-full flex-1 flex-col self-center pl-1">
+              <p className="font-bold">{product.name}</p>
+              <p>{product.price} €</p>
+            </div>
+
+            <div className="flex flex-col items-center justify-end gap-1 self-end">
+              <Tooltip>
+                <TooltipTrigger
+                  asChild
+                  className="h-8 w-8 rounded-md bg-primary-text-1 p-1 text-center text-primary-bg-1"
+                >
+                  <MotionButton
+                    onClick={() => handleBuyButton(product.id)}
+                    className="relative"
+                  >
+                    <p className="absolute bottom-3 right-0.5">+</p>
+                    <ShoppingCart className="h-5 w-5" />
+                  </MotionButton>
+                </TooltipTrigger>
+                <TooltipContent className="rounded-lg bg-primary-bg-3 p-1 text-xs text-primary-text-3">
+                  <p>Zum Warenkorb hinzufügen</p>
+                </TooltipContent>
+              </Tooltip>
+              <div className="flex items-center gap-0.5">
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <MotionButton
+                      onClick={() => handleSetAmountChange(amount - 1)}
+                    >
+                      <Minus className="h-5 w-5" />
+                    </MotionButton>
+                  </TooltipTrigger>
+                  <TooltipContent className="rounded-lg bg-primary-bg-3 p-1 text-xs text-primary-text-3">
+                    <p>Anzahl erhöhen</p>
+                  </TooltipContent>
+                </Tooltip>
+
+                <input
+                  type="number"
+                  value={amount}
+                  onChange={(e) => setAmount(Number.parseInt(e.target.value))}
+                  min={1}
+                  max={10}
+                  className="h-8 w-8 rounded bg-primary-text-1 text-center text-primary-bg-1 [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                />
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <MotionButton
+                      onClick={() => handleSetAmountChange(amount + 1)}
+                      className="text-xl"
+                    >
+                      <Plus className="h-5 w-5" />
+                    </MotionButton>
+                  </TooltipTrigger>
+                  <TooltipContent className="rounded-lg bg-primary-bg-3 p-1 text-xs text-primary-text-3">
+                    <p>Anzahl verringern</p>
+                  </TooltipContent>
+                </Tooltip>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex w-full items-center justify-center bg-primary-bg-2"></div>
-        <p className="font-bold">{product.name}</p>
-        <p>{product.price}€</p>
-        <div className="flex items-center justify-center gap-1">
-          <button
-            onClick={() => handleSetAmountChange(amount - 1)}
-            className="mb-1 h-6 w-6 text-lg transition-colors hover:bg-gray-200"
-          >
-            -
-          </button>
-          <input
-            type="number"
-            value={amount}
-            onChange={(e) => setAmount(Number.parseInt(e.target.value))}
-            min={1}
-            max={10}
-            className="h-6 w-12 text-gray-600 rounded border border-gray-300 text-center [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-          />
-          <button
-            onClick={() => handleSetAmountChange(amount + 1)}
-            className="mb-1 h-6 w-6 self-center text-center align-middle text-lg transition-colors hover:bg-gray-200"
-          >
-            +
-          </button>
-        </div>
-        <Tooltip>
-          <TooltipTrigger asChild className="w-fit">
-            <MotionButton
-              onClick={() => handleBuyButton(product.id)}
-              className="relative"
-            >
-              <p className="absolute -right-2 bottom-3.5 text-accent-2">+</p>
-              <ShoppingCart className="text-accent-2 h-7 w-7" />
-            </MotionButton>
-          </TooltipTrigger>
-          <TooltipContent className="rounded-lg bg-primary-bg-3 p-1 text-xs text-primary-text-3">
-            <p>Zum Warenkorb hinzufügen</p>
-          </TooltipContent>
-        </Tooltip>
-        {/* <ShoppingCart className="h-7 w-7">
-          <button
-            className="rounded-md bg-blue-500 p-2"
-            onClick={() => handleBuyButton(product.id)}
-          >
-            Add to Cart
-          </button>
-        </ShoppingCart> */}
-      </div>
       </TooltipProvider>
     );
   } else {
     return <div className="h-96 w-full bg-primary-bg-3"></div>;
   }
 }
-
 
 export default ProductGridElement;

2d58e37edf709ce1ef13b2bdd94a63f6f57fea4b
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-03-20 15:34:46 +0100

    feat(OrderHistory): add placeholder when empty

--- src/components/orderHistory/OrderHistory.tsx
+++ src/components/orderHistory/OrderHistory.tsx
@@ -5,21 +5,27 @@ function OrderHistory() {
   const { data, isLoading, isError } = useGetOrders();
 
   if (Array.isArray(data) && data.length === 0) {
-    return;
+    return (
+      <div className="my-6 flex w-full max-w-[600px] flex-col items-center justify-center gap-2 self-center px-2">
+        <p className="self-start text-lg font-bold">
+          Deine Bestellhistorie is leer.
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="my-6 flex w-full max-w-[600px] flex-col items-center justify-center gap-2 self-center px-2">
+        <p className="self-start text-lg font-bold">Deine Bestellhistorie</p>
+        {Array.isArray(data) &&
+          data.map((order) => (
+            <OrderHistoryElement
+              order={order}
+              key={`order-history-element-${order.orderNumber}`}
+            />
+          ))}
+      </div>
+    );
   }
-
-  return (
-    <div className="my-6 flex w-full max-w-[600px] flex-col items-center justify-center gap-2 self-center px-2">
-      <p className="self-start text-lg font-bold">Deine Bestellhistorie</p>
-      {Array.isArray(data) &&
-        data.map((order) => (
-          <OrderHistoryElement
-            order={order}
-            key={`order-history-element-${order.orderNumber}`}
-          />
-        ))}
-    </div>
-  );
 }
 
 export default OrderHistory;

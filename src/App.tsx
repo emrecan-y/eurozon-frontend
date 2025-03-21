87edcdc9ae25b1d129333cce7bcbacc38f912b91
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-03-21 10:10:48 +0100

    bugfix(app): change provider structure to fix rerender

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { CookiesProvider } from "react-cookie";
 import AppRoutes from "./AppRoutes";
 import { ToastContainer } from "react-toastify";
 import useDarkMode from "./components/hooks/useDarkMode";
 import { X } from "lucide-react";
 import MotionButton from "./components/ui/MotionButton";
-import { TooltipProvider } from "./components/ui/tooltip";
 
 function App() {
-  const queryClient = new QueryClient();
   const { darkMode } = useDarkMode();
+
+  const stylingHelp = (
+    <div className="flex gap-3">
+      <div className="rounded border-2 bg-primary-bg-1 p-3 text-primary-text-1">
+        <p>Primary 1</p>
+        <p className="font-bold text-accent-1"> Hallo</p>
+      </div>
+      <div className="rounded border-2 bg-primary-bg-2 p-3 text-primary-text-2">
+        <p>Primary 2</p>
+        <p className="font-bold text-accent-2"> Hallo</p>
+      </div>
+      <div className="rounded border-2 bg-primary-bg-3 p-3 text-primary-text-3">
+        <p>Primary 3</p>
+        <p className="font-bold text-accent-3"> Hallo</p>
+      </div>
+    </div>
+  );
 
   return (
     <>
-      <TooltipProvider>
-        <CookiesProvider>
-          <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-              <AppRoutes />
-            </BrowserRouter>
-          </QueryClientProvider>
-        </CookiesProvider>
-      </TooltipProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
       <ToastContainer
         position="bottom-center"
         theme={`${darkMode ? "dark" : "light"}`}
@@ -40,20 +47,6 @@ function App() {
           </MotionButton>
         )}
       />
-      <div className="flex gap-3">
-        <div className="rounded border-2 bg-primary-bg-1 p-3 text-primary-text-1">
-          <p>Primary 1</p>
-          <p className="font-bold text-accent-1"> Hallo</p>
-        </div>
-        <div className="rounded border-2 bg-primary-bg-2 p-3 text-primary-text-2">
-          <p>Primary 2</p>
-          <p className="font-bold text-accent-2"> Hallo</p>
-        </div>
-        <div className="rounded border-2 bg-primary-bg-3 p-3 text-primary-text-3">
-          <p>Primary 3</p>
-          <p className="font-bold text-accent-3"> Hallo</p>
-        </div>
-      </div>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CookiesProvider } from "react-cookie";
+import { TooltipProvider } from "./components/ui/tooltip.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <CookiesProvider>
+        <TooltipProvider>
+          <App />
+        </TooltipProvider>
+      </CookiesProvider>
+    </QueryClientProvider>
   </StrictMode>,
-)
+);

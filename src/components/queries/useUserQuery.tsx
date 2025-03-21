3bfea141b22388581afd820c9dd7234613b5ba2b
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-03-21 10:25:21 +0100

    feat(useUserQuery): add logout on error

--- src/components/queries/useUserQuery.tsx
+++ src/components/queries/useUserQuery.tsx
@@ -1,10 +1,16 @@
 import { getUser } from "@/api/api";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 export function useUserQuery() {
+  const queryClient = useQueryClient();
   const userQuery = useQuery({
     queryKey: ["user"],
     queryFn: getUser,
+    onError: () => {
+      queryClient.clear();
+      toast.error("Sie wurden abgemeldet.");
+    },
   });
   return userQuery;
 }

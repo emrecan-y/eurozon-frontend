6f63344105ae5182fd43ba0e99c423a507fc2954
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-03-21 08:33:24 +0100

    feat(useOrderQuery): add toast onSuccess and onError to mutation

--- src/components/queries/useOrdersQuery.tsx
+++ src/components/queries/useOrdersQuery.tsx
@@ -1,48 +1,30 @@
-
 import { addOrder, getOrders } from "@/api/api";
 import { Order } from "@/models/order";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 export const useGetOrders = () => {
-    return useQuery<Order[], Error>({
-        queryKey: ["orderList"],
-        queryFn: getOrders,
-    });
+  return useQuery<Order[], Error>({
+    queryKey: ["orderList"],
+    queryFn: getOrders,
+  });
 };
-
-// export const useRemoveShoppingCartPosition = () => {
-//     const queryClient = useQueryClient();
-//     return useMutation(
-//         {
-//             mutationFn: removeShoppingCartPosition,
-//             mutationKey: ["shoppingCart"],
-//             onSuccess: () => {
-//                 queryClient.invalidateQueries({ queryKey: ["shoppingCart"] });
-//             },
-//         });
-// };
 
 export const useAddOrders = () => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const mutation = useMutation({
-        mutationFn: addOrder,
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ["orderList"] });
-            queryClient.invalidateQueries({ queryKey: ["shoppingCart"] });
-        },
-    });
-    return mutation;
+  const mutation = useMutation({
+    mutationFn: addOrder,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["orderList"] });
+      queryClient.invalidateQueries({ queryKey: ["shoppingCart"] });
+      toast.success(data);
+    },
+    onError: (error) => {
+      const errorMessage =
+        typeof error === "string" ? error : "An unexpected error occurred.";
+      toast.error(errorMessage);
+    },
+  });
+  return mutation;
 };
-
-// export const useUpdateShoppingCartPosition = () => {
-//     const queryClient = useQueryClient();
-
-//     return useMutation({
-//         mutationFn: updateShoppingCartPosition,
-//         mutationKey: ["shoppingCart"],
-//         onSuccess: () => {
-//             queryClient.invalidateQueries({ queryKey: ["shoppingCart"] });
-//         },
-//     });
-// };

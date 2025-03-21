567de22a2602426358a92d8b8dc681d1e265c8e0
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-03-21 08:26:18 +0100

    feat(app): create toastcontainer, add global tooltip provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,43 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CookiesProvider } from "react-cookie";
 import AppRoutes from "./AppRoutes";
+import { ToastContainer } from "react-toastify";
+import useDarkMode from "./components/hooks/useDarkMode";
+import { X } from "lucide-react";
+import MotionButton from "./components/ui/MotionButton";
+import { TooltipProvider } from "./components/ui/tooltip";
 
 function App() {
   const queryClient = new QueryClient();
+  const { darkMode } = useDarkMode();
 
   return (
     <>
-      <CookiesProvider>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <AppRoutes />
-          </BrowserRouter>
-        </QueryClientProvider>
-      </CookiesProvider>
-
+      <TooltipProvider>
+        <CookiesProvider>
+          <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+              <AppRoutes />
+            </BrowserRouter>
+          </QueryClientProvider>
+        </CookiesProvider>
+      </TooltipProvider>
+      <ToastContainer
+        position="bottom-center"
+        theme={`${darkMode ? "dark" : "light"}`}
+        autoClose={3000}
+        toastClassName={
+          "bg-primary-bg-1 text-primary-text-1 border-primary-bg-3 relative "
+        }
+        closeButton={(props) => (
+          <MotionButton
+            className="absolute right-0 top-0 stroke-primary-text-1"
+            onClick={props.closeToast}
+          >
+            <X />
+          </MotionButton>
+        )}
+      />
       <div className="flex gap-3">
         <div className="rounded border-2 bg-primary-bg-1 p-3 text-primary-text-1">
           <p>Primary 1</p>

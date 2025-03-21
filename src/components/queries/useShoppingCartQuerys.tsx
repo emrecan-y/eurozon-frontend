4039ad2d0872249185f6258ae16f3487a2ce093b
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-03-21 08:33:36 +0100

    feat(useShoppingCartQuery): add toast onSuccess and onError to mutations

--- src/components/queries/useShoppingCartQuerys.tsx
+++ src/components/queries/useShoppingCartQuerys.tsx
@@ -1,46 +1,69 @@
-import { addShoppingCartPosition, getShoppingCartPositions, removeShoppingCartPosition, updateShoppingCartPosition } from "@/api/api";
+import {
+  addShoppingCartPosition,
+  getShoppingCartPositions,
+  removeShoppingCartPosition,
+  updateShoppingCartPosition,
+} from "@/api/api";
 import { ShoppingCartPosition } from "@/models/shoppingcartPosition";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 export const useGetShoppingCartPositions = () => {
-    return useQuery<ShoppingCartPosition[], Error>({
-        queryKey: ["shoppingCart"],
-        queryFn: getShoppingCartPositions,
-    });
+  return useQuery<ShoppingCartPosition[], Error>({
+    queryKey: ["shoppingCart"],
+    queryFn: getShoppingCartPositions,
+  });
 };
 
 export const useRemoveShoppingCartPosition = () => {
-    const queryClient = useQueryClient();
-    return useMutation(
-        {
-            mutationFn: removeShoppingCartPosition,
-            mutationKey: ["shoppingCart"],
-            onSuccess: () => {
-                queryClient.invalidateQueries({ queryKey: ["shoppingCart"] });
-            },
-        });
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: removeShoppingCartPosition,
+    mutationKey: ["shoppingCart"],
+    onSuccess: (data) => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ["shoppingCart"] });
+    },
+    onError: (error) => {
+      const errorMessage =
+        typeof error === "string" ? error : "An unexpected error occurred.";
+      toast.error(errorMessage);
+    },
+  });
 };
 
 export const useAddShoppingCartPosition = () => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const mutation = useMutation({
-        mutationFn: addShoppingCartPosition,
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ["shoppingCart"] });
-        },
-    });
-    return mutation;
+  const mutation = useMutation({
+    mutationFn: addShoppingCartPosition,
+    onSuccess: (data) => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ["shoppingCart"] });
+    },
+    onError: (error) => {
+      const errorMessage =
+        typeof error === "string" ? error : "An unexpected error occurred.";
+      toast.error(errorMessage);
+    },
+  });
+  return mutation;
 };
 
 export const useUpdateShoppingCartPosition = () => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    return useMutation({
-        mutationFn: updateShoppingCartPosition,
-        mutationKey: ["shoppingCart"],
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ["shoppingCart"] });
-        },
-    });
+  return useMutation({
+    mutationFn: updateShoppingCartPosition,
+    mutationKey: ["shoppingCart"],
+    onSuccess: (data) => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ["shoppingCart"] });
+    },
+    onError: (error) => {
+      const errorMessage =
+        typeof error === "string" ? error : "An unexpected error occurred.";
+      toast.error(errorMessage);
+    },
+  });
 };

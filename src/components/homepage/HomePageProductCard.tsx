f404cccec2b73d68023c548828dea59b4d0f9c67
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-03-21 08:29:04 +0100

    refactor(HomaPage): remove tooltip proivder

--- src/components/homepage/HomePageProductCard.tsx
+++ src/components/homepage/HomePageProductCard.tsx
@@ -3,12 +3,14 @@ import {
   Tooltip,
   TooltipTrigger,
   TooltipContent,
-  TooltipProvider,
 } from "@radix-ui/react-tooltip";
 import { ShoppingCart } from "lucide-react";
 import MotionButton from "../ui/MotionButton";
 import { UUID } from "crypto";
 import { useAddShoppingCartPosition } from "../queries/useShoppingCartQuerys";
+import { useUserQuery } from "../queries/useUserQuery";
+import { useCookies } from "react-cookie";
+import { toast } from "react-toastify";
 
 type HomePageProductCardProps = {
   product: Product;
@@ -16,45 +18,47 @@ type HomePageProductCardProps = {
 
 function HomePageProductCard({ product }: HomePageProductCardProps) {
   const addMutation = useAddShoppingCartPosition();
+  const userQuery = useUserQuery();
+  const [cookies, setCookie, removeCookie] = useCookies(["access_token"]);
 
   const handleBuyButton = (productId: UUID) => {
-    addMutation.mutate({ productId, amount: 1 });
+    if (userQuery.data?.id && cookies.access_token) {
+      addMutation.mutate({ productId, amount: 1 });
+    } else {
+      toast.error("Für diese Funktion müssen Sie sich anmelden.");
+    }
   };
 
   return (
-    <TooltipProvider>
-      <div className="p-4">
-        <div className="flex h-56 w-32 flex-col items-center justify-around gap-1 rounded-lg border border-primary-bg-3 bg-primary-bg-2 p-4 text-center shadow-lg lg:h-72 lg:w-56">
-          <img
-            src={product.imageUrl}
-            alt={product.name}
-            className="h-32 object-cover"
-          />
-          <h3 className="mt-2 text-sm font-semibold lg:text-base">
-            {product.name}
-          </h3>
-          <div className="flex flex-row gap-2 text-sm lg:text-base">
-            <p className="mt-1 font-bold">{product.price}€</p>
-            <Tooltip>
-              <TooltipTrigger asChild className="w-fit">
-                <MotionButton
-                  onClick={() => handleBuyButton(product.id)}
-                  className="relative"
-                >
-                  <p className="absolute -right-2 bottom-3.5 text-accent-2">
-                    +
-                  </p>
-                  <ShoppingCart className="h-5 w-5 text-accent-2" />
-                </MotionButton>
-              </TooltipTrigger>
-              <TooltipContent className="rounded-lg bg-primary-bg-3 p-1 text-xs text-primary-text-3">
-                <p>Zum Warenkorb hinzufügen</p>
-              </TooltipContent>
-            </Tooltip>
-          </div>
+    <div className="p-4">
+      <div className="flex h-56 w-32 flex-col items-center justify-around gap-1 rounded-lg border border-primary-bg-3 bg-primary-bg-2 p-4 text-center shadow-lg lg:h-72 lg:w-56">
+        <img
+          src={product.imageUrl}
+          alt={product.name}
+          className="h-32 object-cover"
+        />
+        <h3 className="mt-2 text-sm font-semibold lg:text-base">
+          {product.name}
+        </h3>
+        <div className="flex flex-row gap-2 text-sm lg:text-base">
+          <p className="mt-1 font-bold">{product.price}€</p>
+          <Tooltip>
+            <TooltipTrigger asChild className="w-fit">
+              <MotionButton
+                onClick={() => handleBuyButton(product.id)}
+                className="relative"
+              >
+                <p className="absolute -right-2 bottom-3.5 text-accent-2">+</p>
+                <ShoppingCart className="h-5 w-5 text-accent-2" />
+              </MotionButton>
+            </TooltipTrigger>
+            <TooltipContent className="rounded-lg bg-primary-bg-3 p-1 text-xs text-primary-text-3">
+              <p>Zum Warenkorb hinzufügen</p>
+            </TooltipContent>
+          </Tooltip>
         </div>
       </div>
-    </TooltipProvider>
+    </div>
   );
 }
 

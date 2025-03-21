bcce56c2300c3159e8655de83040c3ed7078fb2d
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-03-21 08:32:23 +0100

    feat(ShopLayout): style layout

--- src/components/layouts/ShopLayout.tsx
+++ src/components/layouts/ShopLayout.tsx
@@ -6,12 +6,12 @@ function ShopLayout() {
   return (
     <div className="flex min-h-screen flex-col bg-primary-bg-1 text-primary-text-1 transition-colors duration-100">
       <Navbar />
-      <div className="flex h-full flex-1">
+      <div className="flex flex-1">
         <div className="hidden bg-primary-bg-2 text-primary-text-2 md:flex md:flex-col md:items-start md:gap-1 md:p-2 md:pt-1">
           <p className="font-bold">Kategorien</p>
           <NavBarCategories />
         </div>
-        <div className="flex h-full flex-1 flex-col">
+        <div className="flex min-h-full flex-1 flex-col">
           <Outlet />
         </div>
       </div>

35193def791f3062c9d71ce27a290c7ba8c156d4
emrecan-y <emrecanyilmaz1997@gmail.com>
2025-03-26 19:42:18 +0100

    feat(NavBarCategories): better state handling by pathname

--- src/components/navbar/NavBarCategories.tsx
+++ src/components/navbar/NavBarCategories.tsx
@@ -1,10 +1,25 @@
 import { useLocation } from "react-router-dom";
 import { useMainCategoriesQuery } from "../../queries/useMainCategoriesQuery";
 import NavBarCategoryButton from "./NavBarCategoryButton";
+import { useEffect, useRef, useState } from "react";
 
 function NavBarCategories() {
   const mainCategoriesQuery = useMainCategoriesQuery();
-  const location = useLocation();
+  const [lastTimeStamp, setLastTimeStamp] = useState(Date.now());
+
+  const { pathname } = useLocation();
+  const lastPathRef = useRef("");
+
+  // only rerender the NavBarCategories when moving out of products
+  useEffect(() => {
+    if (
+      (lastPathRef.current === "/products" && pathname !== "/products") ||
+      (pathname !== "/products" && lastPathRef.current !== pathname)
+    ) {
+      setLastTimeStamp(Date.now());
+    }
+    lastPathRef.current = pathname;
+  }, [pathname]);
 
   return (
     <>
@@ -13,7 +28,7 @@ function NavBarCategories() {
         mainCategoriesQuery.data.map((category) => (
           <>
             <NavBarCategoryButton
-              key={`nav-button-${category.name.toLowerCase()}-${location.pathname}`}
+              key={`nav-button-${category.name.toLowerCase()}-${lastTimeStamp}`}
               category={category}
             />
           </>
